d8186a1b30b86b5f1b4aed5f59d932e0f80987ae
Jan Thiel <JanThiel@users.noreply.github.com>
2019-02-07 12:09:26 +0100

    More comments about what's going on

--- src/main/java/de/tk/opensource/privacyproxy/retrieval/AssetRetrievalService.java
+++ src/main/java/de/tk/opensource/privacyproxy/retrieval/AssetRetrievalService.java
@@ -1,4 +1,4 @@
-/*--- (C) 1999-2017 Techniker Krankenkasse ---*/
+/*--- (C) 1999-2019 Techniker Krankenkasse ---*/
 
 package de.tk.opensource.privacyproxy.retrieval;
 
@@ -16,16 +16,34 @@ import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
+/**
+* This class downloads files from a remote host and stores them grouped by "provider" on the file System.
+* You can download any arbitrary file. If the endpoint is a ZIP it will be extracted and its content will be stored.
+* Please implement your own Service per provider. See the ExampleRetrievalScriptService for an example.
+* The regular fetch is done by the Spring Boot cron system which is setup in your provider retrieval class.
+*
+* All configs for the retrieval and your providers should be done in the application.yml within the Application
+*/
 public abstract class AssetRetrievalService implements InitializingBean {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(AssetRetrievalService.class);
 
+	/**
+	* Configure where the files will be stored on the file system.
+	*/
 	@Value("${assets.fileLocation}")
 	private String location;
 
 	@Autowired
 	private Proxy proxy;
 
+	/**
+	* Method to fetch the assets defined in the proper implementation class. You need to implement one AssetRetrievalService class per Provider.
+	* A provider is a logical group which should represent the source of the files.
+	* e.g. a "google-fonts" provider would be a useful case
+	* The endpoints can be configured as a single URL or a list of comma separated URLs. Put them into the application.yml
+	* If the endpoint delivers a ZIP file it will be extracted
+	*/
 	public void updateAssetsInternal(String provider, List<String> endpoints) {
 		boolean directoryAvailable;
 		File directory = new File(location + "/" + provider);
@@ -38,8 +56,12 @@ public abstract class AssetRetrievalService implements InitializingBean {
 			try {
 				LOGGER.info("Updating the {} assets", provider);
 				for (String endpoint : endpoints) {
+					// Access the endpoint with a cachebuster to prevent network middleware (corporate proxies) to cache the response
 					URL url = new URL(endpoint + "?_=" + System.currentTimeMillis());
 					URLConnection connection = url.openConnection(proxy);
+					// If the endpoint delivers ZIP, extract it.
+					// TODO: Allow configuration per endpoint to force extract. 
+					// The filename might not be enough (Google Fonts Downloads e.g. do not resolve here)
 					if (endpoint.endsWith(".zip")) {
 						try(
 							ZipInputStream inputStream =
@@ -76,6 +98,9 @@ public abstract class AssetRetrievalService implements InitializingBean {
 		this.updateAssets();
 	}
 
+	/**
+	* Writes the retrieved files to disk
+	*/
 	public void writeFile(InputStream inputStream, String provider, String fileName) {
 		byte[] buffer = new byte[1024];
 		try(
@@ -93,5 +118,3 @@ public abstract class AssetRetrievalService implements InitializingBean {
 	}
 
 }
-
-/*--- Formatiert nach TK Code Konventionen vom 05.03.2002 ---*/
