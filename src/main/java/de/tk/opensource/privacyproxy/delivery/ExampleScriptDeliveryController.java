b34fdc5f6ed17d40dfdd0ce58a715fdc68379a73
Jan Thiel <JanThiel@users.noreply.github.com>
2019-02-07 12:14:41 +0100

    More comments about what's going on

--- src/main/java/de/tk/opensource/privacyproxy/delivery/ExampleScriptDeliveryController.java
+++ src/main/java/de/tk/opensource/privacyproxy/delivery/ExampleScriptDeliveryController.java
@@ -13,9 +13,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.nio.charset.Charset;
 
+/**
+* This is an example implementation of the AssetDeliveryController
+* Please implement your own DeliveryControllers per file type. Dependant on what should be delivered, a specific
+* FontDeliveryController could be another useful delivery.
+*/
 @Controller
 public class ExampleScriptDeliveryController extends AssetDeliveryController {
 
+	/**
+	* Sets up Cachegroups as well as the URL mapping under which this content should be accessible.
+	* Make sure to set the both following annotations properly to your usecase.
+	* Also ensure that the URL mapping is unique. Add a slug like /js/{provider...}/{script...} or /fonts/....
+	*/
 	@Cacheable(
 		cacheNames = "js",
 		key = "#provider + #script"
