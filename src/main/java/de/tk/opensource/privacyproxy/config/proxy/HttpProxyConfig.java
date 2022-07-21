68824e527ee3cccc3d47e7664e53ccd3422dccc6
Kassim Hölting <P225828@tk.de>
2022-07-21 09:31:46 +0200

    using null values

--- src/main/java/de/tk/opensource/privacyproxy/config/proxy/HttpProxyConfig.java
+++ src/main/java/de/tk/opensource/privacyproxy/config/proxy/HttpProxyConfig.java
@@ -8,17 +8,16 @@ import java.net.Proxy;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.StringUtils;
 
 @Configuration
 public class HttpProxyConfig {
 
 	@Bean
 	public Proxy proxy(
-		@Value("${http.proxyHost:''}") String proxyHost,
-		@Value("${http.proxyPort:-1}") Integer proxyPort
+		@Value("${http.proxyHost:#{null}}") String proxyHost,
+		@Value("${http.proxyPort:#{null}") Integer proxyPort
 	) {
-		if (!StringUtils.hasText(proxyHost) || proxyPort == -1) {
+		if (proxyHost == null || proxyPort == null) {
 			return Proxy.NO_PROXY;
 		} else {
 			return new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyHost, proxyPort));
